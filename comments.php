a89ba28c924f91ec9d89e3cdee7ac4c498aa2a0b
Lindsay Macvean <lindsaymacvean@gmail.com>
2013-06-28 13:50:24 +0100

    edited to grab the whole thing in 1 sql query using a FULL JOIN

--- comments.php
+++ comments.php
@@ -3,44 +3,35 @@
  * Find the post with the most comments on WordPress
  * Copyright 2013 - Shane Curran
  */
+ 
+//start a session
+session_start();
 
 // Load in the functions file
 require_once "inc/functions.inc.php";
 start_html(array("comments.css"));
 
      // Include the wp-config.php file
-    require_once "wp-config.php";
+    require_once "../wp-config.php";
 
     // Eww, mysql_connect(). Remind me to replace this with PDO later
     $database = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
                             mysql_select_db(DB_NAME);
 
     // Select all the posts from the database
-    $posts_query = mysql_query("SELECT `ID`, `post_title` FROM `wp_posts`");
+    $posts_query = mysql_query("SELECT 'ID', 'post_title', count('comment_ID`)  FROM 'wp_posts' FULL JOIN 'wp_comments' ON 'wp_posts.ID'='wp_comments.comment_post_ID' ORDER BY 'comment_post_ID' ASC ");
 
     // Run a loop to fetch each row retrieved in the above query and store it in an array
     while ($row = mysql_fetch_assoc($posts_query)) {
         $posts[] = $row;
     }
 
-    $comments_count = array();
-
-    // Run a loop to query the database to find how many comments each post has
-    foreach ($posts as $key => $value) {
-       $query  = mysql_query(sprintf("SELECT count(`comment_ID`) AS `comments` FROM `wp_comments` WHERE `comment_post_ID` = '%s'", $value['ID']));
-       $results = mysql_fetch_assoc($query);
-
-       $comments_count[] = array("count" => $results['comments'],
-                                                    "post"   => $value['post_title']);
-    }
-
-    // Find the post with the most comments
-    $most = max($comments_count);
-
     // Return this awesome information to the end-user :)
-    div(array(), array("result"));
-        h("2", "The most commented on post is \"" . b($most['post']) . "\" which had a total of " . b($most['count']) . " comments");
-    close_div();
-
+    while($comments_count['count']) {
+		div(array(), array("result"));
+			h("2", "The Post \"" . b($posts[2]) . "\" had a total of " . b($posts[3]) . " comments");
+		close_div();
+	}
+		
 end_html();
 ?>
